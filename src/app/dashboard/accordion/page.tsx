50f16c063260237d2286c05ab2e4bb612b67ec14
BIEH01 <isaacescamilla.h1998@gmail.com>
2024-12-28 20:42:28 -0600

    The accordion and alert component were created

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,39 @@
-export default function Page() {
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const data = [
+	{
+		id: "1",
+		question: "Is it accessible?",
+		answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+	},
+	{
+		id: "2",
+		question: "Is it styled?",
+		answer:
+			"Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+	},
+	{
+		id: "3",
+		question: "Is it animated?",
+		answer:
+			"Yes. It&apos;s animated by default, but you can disable it if you prefer.",
+	},
+];
+
+export default function AccordionDemo() {
 	return (
-		<div>
-			<h1>Accordion page</h1>
-		</div>
+		<Accordion type="single" collapsible className="w-full">
+			{data.map((item) => (
+				<AccordionItem value={item.id} key={item.id}>
+					<AccordionTrigger>{item.question}</AccordionTrigger>
+					<AccordionContent>{item.answer}</AccordionContent>
+				</AccordionItem>
+			))}
+		</Accordion>
 	);
 }
